a6510f7dea9d260cef5f1ab991d9ea9400f856e7
Aman-Najibeh <amannj.1997@gmail.com>
2023-01-01 14:58:27 +0200

    befor bulma

---
+++ src/store/app.js
@@ -0,0 +1,16 @@
+// Utilities
+import { defineStore } from 'pinia'
+
+export const useAppStore = defineStore('app', {
+  state: () => ({
+    //
+    
+  }),
+  getters:{
+    //computed
+  },
+  actions:{
+    //method 
+  }
+
+})
