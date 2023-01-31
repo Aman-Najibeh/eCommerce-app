4c6a84516c92caa1ab8f1fecf1632fa7454cc762
Aman-Najibeh <amannj.1997@gmail.com>
2023-01-31 22:36:13 +0200

    adding dist

---
+++ assets/NotFoundView.f6d84b70.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,f as o,a as t,a8 as a}from"./index.4d9cef6d.js";const c="/eCommerce-app/assets/undraw_page_not_found_re_e9o6.4a4915c2.svg",n={},r={class:"w-75 h-75 mx-16 mt-4"};function _(p,m,d,f,i,l){return s(),o("section",r,[t(a,{src:c,class:"w-100 mx-16 pl-16"})])}const x=e(n,[["render",_]]);export{x as default};
