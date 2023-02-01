c4bef66895528442143e06cb20fc7ec14676e9cf
Aman-Najibeh <amannj.1997@gmail.com>
2023-02-01 22:25:31 +0200

    edit design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,24 @@
 <template>
- <nav-bar-section></nav-bar-section>
- <hero-section></hero-section>
- <features-section></features-section>
- <testimonial-section></testimonial-section>
+  <section>
+    <nav-bar-section></nav-bar-section>
+    <hero-section></hero-section>
+    <features-section></features-section>
+    <testimonial-section></testimonial-section>
+  </section>
 </template>
 
-<script >
-import HeroSection from '@/components/HeroSection.vue';
-import FeaturesSection from '@/components/featuresSection.vue';
+<script>
+import HeroSection from "@/components/HeroSection.vue";
+import FeaturesSection from "@/components/featuresSection.vue";
 // import OurTeamSection from '@/components/OurTeamSection.vue';
-import TestimonialSection from '@/components/TestimonialSection.vue';
-import NavBarSection from '@/components/NavBarSection.vue';
+import TestimonialSection from "@/components/TestimonialSection.vue";
+import NavBarSection from "@/components/NavBarSection.vue";
 export default {
-  components: { HeroSection, FeaturesSection, NavBarSection ,TestimonialSection},
-}
- 
+  components: { HeroSection, FeaturesSection, NavBarSection, TestimonialSection },
+};
 </script>
 <style scoped>
-*{
-  margin: 0 ;
+* {
+  margin: 0;
 }
 </style>
