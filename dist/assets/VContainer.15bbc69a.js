8594b38941c0ea512b7b5a82d799dbd235091e2a
Aman-Najibeh <amannj.1997@gmail.com>
2023-02-01 19:34:41 +0200

    edit config

--- dist/assets/VContainer.15bbc69a.js
+++
@@ -1,1 +0,0 @@
-import"./VRow.380e6052.js";import{g as n,m as o,u as s,a as r}from"./index.04a87902.js";const u=n({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...o()},setup(e,a){let{slots:t}=a;return s(()=>r(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},t)),{}}});export{u as V};
