4f5e3a3d5c753e90a576105ad0607cff23998c4e
Aman-Najibeh <amannj.1997@gmail.com>
2023-02-02 15:31:17 +0200

    adding dist

---
+++ assets/ssrBoot.227b0202.js
@@ -0,0 +1,1 @@
+import{n as t,y as e,l as s,ab as n}from"./index.79b8e9cd.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
