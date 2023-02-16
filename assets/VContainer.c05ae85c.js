de30ed7b33391d319c9dfe7bf7276bd662de31b7
Aman-Najibeh <amannj.1997@gmail.com>
2023-02-16 21:50:31 +0200

    add dist

---
+++ assets/VContainer.c05ae85c.js
@@ -0,0 +1,1 @@
+import"./VRow.2c1998e9.js";import{g as n,m as o,u as s,a as r}from"./index.1756445f.js";const u=n({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...o()},setup(e,a){let{slots:t}=a;return s(()=>r(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},t)),{}}});export{u as V};
